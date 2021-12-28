98215cb819df78903305fc3ba5cb708b2e4eeea0
AhmedMustafaEl-Zahaby <76495943+AhmedMustafaEl-Zahaby@users.noreply.github.com>
2021-12-28 17:22:28 +0200

    Add files via upload

---
+++ Queuing/src/fin/Fin.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fin;
+import java.awt.*;
+import syntex.Home;
+public class Fin {
+    public static void main(String[] args) {
+        Home ob = new Home();
+        ob.setVisible(true);
+    }
+    
+}
